712d1d77008799badc2bc6e6ed284018da9c7f52
Roman Shcherbich <r_shcherbich@wargaming.net>
2022-02-14 20:22:57 +0300

    allure demo

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,9 +3,9 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.annotations.*;
-import utils.RetryAnalyzer;
+import utils.TestListener;
 import web.pages.CartPage;
 import web.pages.LoginPage;
 import web.pages.CatalogPage;
@@ -13,7 +13,7 @@ import java.io.File;
 import java.time.Duration;
 import java.util.HashMap;
 
-//@Listeners({utils.RetryAnalyzer.class})
+@Listeners({TestListener.class})
 public class BaseTest {
 
     protected WebDriver driver;
@@ -28,16 +28,8 @@ public class BaseTest {
     public static final File RESOURCE_PATH_FILE = new File("src/test/resources");
     public static final String ABSOLUTE_RESOURCE_PATH = RESOURCE_PATH_FILE.getAbsolutePath();
 
-
-//    @Parameters ( {"username", "password"} )
-//    @BeforeSuite(groups = {"config"})
-//    public void addParams(String username, String password) {
-//        USERNAME = username;
-//        PASSWORD = password;
-//    }
-
     @BeforeMethod(groups = {"config"})
-    public void setup(){
+    public void setup(ITestContext iTestContext){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
@@ -49,10 +41,15 @@ public class BaseTest {
         chromeOptions.setExperimentalOption("prefs", chromePrefs);
 
         driver = new ChromeDriver(chromeOptions);
+        setContextAttribute(iTestContext, "driver", driver);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         loginPage = new LoginPage(driver);
         catalogPage = new CatalogPage(driver);
         cartPage = new CartPage(driver);
+    }
+
+    private void setContextAttribute(ITestContext iTestContext, String attributeKey, Object attributeValue) {
+        iTestContext.setAttribute(attributeKey, attributeValue);
     }
 
     @AfterMethod(alwaysRun=true, groups = {"config"})
--- src/test/java/smoke/LoginTest.java
+++ src/test/java/smoke/LoginTest.java
@@ -1,21 +1,41 @@
 package smoke;
 
 import base.BaseAuthenticatedTest;
+import io.qameta.allure.*;
 import org.testng.Assert;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import utils.RetryAnalyzer;
 import web.pages.LoginPage;
+
+import java.util.Random;
 
 public class LoginTest extends BaseAuthenticatedTest {
 
+    @Step("Second step {keyword}")
+    public void productPageOpenedStep(String keyword) {
+        if (new Random().nextInt(2) == 1) {
+            Assert.fail(" Искусственный провал теста");
+        }
+    }
+
+    @Step("Third step")
+    public void accountCheck(){
+        System.out.println(" accountCheck ");
+    }
+
     @Test
 //    @Test(retryAnalyzer = RetryAnalyzer.class)
-    public void validCredentialsLoginTest() {
-
+    @Parameters({"keyword"})
+    @Flaky
+    @Severity(SeverityLevel.BLOCKER)
+    public void validCredentialsLoginTest(@Optional String keyword) {
         System.out.println("validCredentialsLoginTest");
         validLogin();
+        productPageOpenedStep(keyword);
+        accountCheck();
     }
+
 
     @Test(testName = "NOT")
     public void USERNAME_PLACEHOLDER_TEST() {
