6077cb29793faea8c985b48b0c1da0398e685ca1
Roman Shcherbich <r_shcherbich@wargaming.net>
2022-02-21 20:02:08 +0300

    add CapabilitiesGenerator

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
+import utils.CapabilitiesGenerator;
 import utils.TestListener;
 import web.pages.CartPage;
 import web.pages.LoginPage;
@@ -25,22 +26,9 @@ public class BaseTest {
     public static String PASSWORD = "secret_sauce";
 
 
-    public static final File RESOURCE_PATH_FILE = new File("src/test/resources");
-    public static final String ABSOLUTE_RESOURCE_PATH = RESOURCE_PATH_FILE.getAbsolutePath();
-
     @BeforeMethod(groups = {"config"})
     public void setup(ITestContext iTestContext){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--ignore-popup-blocking");
-        chromeOptions.addArguments("--ignore-certificate-errors");
-
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("download.default_directory", ABSOLUTE_RESOURCE_PATH);
-        chromeOptions.setExperimentalOption("prefs", chromePrefs);
-
-        driver = new ChromeDriver(chromeOptions);
+        driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         setContextAttribute(iTestContext, "driver", driver);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         loginPage = new LoginPage(driver);
